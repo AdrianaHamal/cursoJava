6c3dbd94d6c9b6f119d07492841253296837b68f
Adriana Hamal <ahamal85@gmail.com>
2020-02-03 14:15:33 -0300

    Curso Java

---
+++ src/main/java/curso/Exercicio10.java
@@ -0,0 +1,20 @@
+package curso;
+
+import java.io.InputStream;
+import java.util.Scanner;
+
+public class Exercicio10 {
+
+	public static void main(String[] args) {
+		
+		for (int i=4; i>=0; i--) {
+		     System.out.println("Valor de i: "+ i);
+		}
+		
+		
+
+	}
+	
+}
+
+
---
+++ src/main/java/curso/ExercicioUri1078.java
@@ -0,0 +1,27 @@
+package curso;
+
+import java.util.Scanner;
+
+public class ExercicioUri1078 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner (System.in);
+		
+		int N = sc.nextInt();
+		
+		for (int i=1; i<=10; i++) {
+		    int produto = i * N;
+		    System.out.println(i + " x " + N +" = " + produto);
+		
+		}
+		
+		
+		
+		
+		
+		
+		sc.close();
+	}
+
+}
